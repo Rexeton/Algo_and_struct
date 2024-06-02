80dacff111ba060de789a72c5b13464b75958983
SedynovIvan <SedynovIS@gmail.com>
2024-06-02 15:33:32 +0300

    Лаб 1 выполнил очередь и скобки

--- O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
+++ O-нотация. Простые структуры данных/Лабораторная 1/Скобочные группы/task.py
@@ -6,8 +6,21 @@ def check_brackets(brackets_row: str) -> bool:
     :return: True, если последовательность корректна, False в противном случае
     """
     ...  # TODO реализовать проверку скобочной группы
+    if len(brackets_row)%2!=0:
+        return False
+    count_of_skobki=0
 
+    for el in brackets_row:
+        if el=='(':
+            count_of_skobki+=1
+        else:
+            count_of_skobki -= 1
+        if count_of_skobki<0:
+            return False
+    return count_of_skobki==0
 
 if __name__ == '__main__':
     print(check_brackets("()()"))  # True
     print(check_brackets(")("))  # False
+
+
