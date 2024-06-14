25173681926fbca1cfb78a2bf32027f2b7c82f27
SedynovIvan <SedynovIS@gmail.com>
2024-06-14 21:47:17 +0300

    Лаб 1 выполнил очередь и скобки Лаб 2 сделал

--- Сортировки/Практические задания/Сортировка слиянием/task.py
+++ Сортировки/Практические задания/Сортировка слиянием/task.py
@@ -1,3 +1,4 @@
+import random
 from typing import List
 
 
@@ -13,3 +14,65 @@ def sort(container: List[int]) -> List[int]:
     :return: Отсортированный в порядке возрастания массив
     """
     ...  # TODO реализуйте сортировку слиянием
+    if len(container)<=1:
+        return container
+    midle=len(container)//2
+    left_conteiner=sort(container[:midle])
+    right_conteiner=sort(container[midle:])
+    l_ind,r_ind,merge_ind=0,0,0
+    while l_ind<len(left_conteiner) and r_ind<len(right_conteiner):
+        if left_conteiner[l_ind]<=right_conteiner[r_ind]:
+            container[merge_ind]=left_conteiner[l_ind]
+            l_ind+=1
+        else:
+            container[merge_ind]=right_conteiner[r_ind]
+            r_ind+=1
+        merge_ind += 1
+    while l_ind < len(left_conteiner):
+        container[merge_ind] = left_conteiner[l_ind]
+        l_ind += 1
+        merge_ind += 1
+    while r_ind<len(right_conteiner):
+        container[merge_ind]=right_conteiner[r_ind]
+        r_ind += 1
+        merge_ind += 1
+    return container
+
+
+    # if len(container)<=1:
+    #     return container
+    # midle=len(container)//2
+    # left_conteiner=sort(container[:midle])
+    # right_conteiner=sort(container[midle:])
+    # merge_container=[None]*len(container)
+    # l_ind,r_ind,merge_ind=0,0,0
+    # while l_ind<len(left_conteiner) and r_ind<len(right_conteiner):
+    #     if left_conteiner[l_ind]<=right_conteiner[r_ind]:
+    #         merge_container[merge_ind]=left_conteiner[l_ind]
+    #         l_ind+=1
+    #     else:
+    #         merge_container[merge_ind]=right_conteiner[r_ind]
+    #         r_ind+=1
+    #     merge_ind += 1
+    # while l_ind < len(left_conteiner):
+    #     merge_container[merge_ind] = left_conteiner[l_ind]
+    #     l_ind += 1
+    #     merge_ind += 1
+    # while r_ind<len(right_conteiner):
+    #     merge_container[merge_ind]=right_conteiner[r_ind]
+    #     r_ind += 1
+    #     merge_ind += 1
+    # return merge_container
+
+# a=[random.randint(-100, 100) for _ in range(30)]
+# b=sort(a)
+# c=True
+# i=0
+# a=sorted(a)
+# for el in a:
+#    if el!=b[i]:
+#        c=False
+#    i+=1
+# print(c)
+# print(b)
+# print(sorted(a))
--- Сортировки/Самостоятельная работа/Сортировка цвета/task.py
+++ Сортировки/Самостоятельная работа/Сортировка цвета/task.py
@@ -5,11 +5,26 @@ def sort_сolors(nums: List[int]) -> None:
     """
     Ничего не возвращайте, вместо этого измените nums на месте.
     """
-    for el in nums:
-        di[el]+=1
-    res=[]
-    for el in di:
-        res+=[el]*di[el]
-    nums=res
+    if len(nums)==1:
+        return nums
+    min_num = nums[0]
+    max_num = nums[0]
+    ...
+    while c<len(nums):
+        if nums[i]!=0:
+            nums[i_max], nums[i] = nums[i], nums[i_max]
+        else:
+            i_min+=1
+        c+=1
+    return nums
 
-print(sort_сolors([2,0,2,1,1,0]))
+
+print(sort_сolors([1,2]))
+print(sort_сolors([1,0,1]))
+print(sort_сolors([2,0]))
+print(sort_сolors( [2,0,2,1,1,0]))
+print(sort_сolors( [1, 2, 0]))
+print(sort_сolors( [2, 2, 2, 1, 1, 0, 0, 0]))
+print(sort_сolors([2,0,1]))
+
+
