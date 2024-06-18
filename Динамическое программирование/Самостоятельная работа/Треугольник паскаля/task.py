32c8cda585fb6bff1cc86122b52ec40bda221b51
SedynovIvan <SedynovIS@gmail.com>
2024-06-18 21:39:55 +0300

    Лаб 1 выполнил очередь и скобки Лаб 2 сделал Лаб 3 в процессе

--- Динамическое программирование/Самостоятельная работа/Треугольник паскаля/task.py
+++ Динамическое программирование/Самостоятельная работа/Треугольник паскаля/task.py
@@ -3,3 +3,20 @@ from typing import List
 
 def generate(num_rows: int) -> List[List[int]]:
     ...
+    n=num_rows+1
+    res=[]
+    for i in range(1,n):
+        dop=[1 for _ in range(i)]
+        for j in range(1,(i//2)):
+            el=res[-1][j-1]+res[-1][j]
+            dop[j]=el
+            dop[-j -1]=el
+        if i%2==1 and i>2 :
+            col=i//2
+            dop[col]=res[-1][col-1]+res[-1][col]
+        res.append(dop)
+
+    return res
+
+
+print(generate(5))
--- Динамическое программирование/Самостоятельная работа/Ход коня. Ленивая динамика/task.py
+++ Динамическое программирование/Самостоятельная работа/Ход коня. Ленивая динамика/task.py
@@ -10,8 +10,32 @@ def calculate_paths(shape: Tuple[int, int]) -> int:
     :return: количество путей согласно заданным условиям
     """
     ... # TODO реализуйте подсчет ходов коня
+    n=shape[0]-1
+    m = shape[1]-1
+    d = []
+    for j in range(n+1):
+        a2 = []
+        for i in range(m+1):
+            a2.append(-1)
+        d.append(a2)
+    return requrc(n,m,n,m,d)
+
+def requrc(x,y,n,m,d):
+    if (x<0 or y<0 or x>n or y>m):
+        return 0
+    if (x==0 and y==0):
+        return 1
+    if d[x][y]!=-1:
+        return d[x][y]
+
+    d[x][y]=requrc(x-2,y-1,n,m,d)\
+            +requrc(x-2,y+1,n,m,d)\
+            +requrc(x-1,y-2,n,m,d)\
+            +requrc(x+1,y-2,n,m,d) \
 
 
+    return d[x][y]
 if __name__ == '__main__':
+
     print(calculate_paths((4, 4)))  # 2
     print(calculate_paths((7, 15)))  # 13309
