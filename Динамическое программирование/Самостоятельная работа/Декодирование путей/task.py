50c92e83eb99d05e9672eb19aec3931f03a63eda
Иван <SedynovIS@gmail.com>
2024-06-23 22:48:08 +0300

    Работа по динамическому программированию

--- Динамическое программирование/Самостоятельная работа/Декодирование путей/task.py
+++ Динамическое программирование/Самостоятельная работа/Декодирование путей/task.py
@@ -1,2 +1,20 @@
 def num_decodings(s: str) -> int:
     ...
+    d={str(i) for i in range(1,27)}
+    def req(s):
+        i=1
+        while i<len(s):
+            if f'{s[i-1]}{s[i]}' not in d:
+                return req(s[:i])*req(s[i:])
+            i+=1
+        if len(s)==0:
+            return 1
+        if len(1)==1:
+            if s[0] in d:
+                return 1
+            return 0
+
+
+    return req(s)
+
+print(num_decodings("11106"))
--- Динамическое программирование/Самостоятельная работа/Самая длинная несовпадающая подпоследовательность/task.py
+++ Динамическое программирование/Самостоятельная работа/Самая длинная несовпадающая подпоследовательность/task.py
@@ -1,5 +1,5 @@
 from typing import List
-
+from functools import lru_cache
 
 def is_subsequence(s: str, t: str) -> bool:
     ...
@@ -10,19 +10,51 @@ def is_subsequence(s: str, t: str) -> bool:
 
 
     if len(s)==0:
-        return True
+        return 1
     while i<lt:
         if t[i]==s[j]:
             j+=1
         if j==ls:
-            return True
+            return 1
         i+=1
-    return False
+    return 0
 
 
 
 def find_lus_length(strs: List[str]) -> int:
     ...
+    s=list(sorted(strs,key=len,reverse=True))
+    for i in range(1,len(s)):
+        if is_subsequence(s[i],s[i-1])==0:
+            return len(s[i])
+    return -1
+s=  ["aabbcc", "aabbcc","bc","bcc","aabbccc"]
+print(find_lus_length(s))
 
 
+def findLUSlength(strs: List[str]) -> int:
+    ss = list(sorted(strs, key=len, reverse=True))
+    for i in range(1, len(ss)):
+        b = 0
+        s = ss[i]
+        t = ss[i-1]
+        ls = len(s)
+        lt = len(t)
+        if len(s) == 0:
+            b = 1
+        ii = 0
+        j = 0
+        while ii < lt:
+            if t[ii] == s[j]:
+                j += 1
+            if j == ls:
+                b = 1
+                break
+            ii += 1
 
+        if b == 0:
+            return len(ss[i])
+    return -1
+
+
+print(findLUSlength(s))
