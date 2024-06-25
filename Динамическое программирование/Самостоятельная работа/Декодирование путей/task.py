9aa0ccff66aa73b75256282719c217a15e53fbba
Иван <SedynovIS@gmail.com>
2024-06-25 22:21:58 +0300

    Работа по динамическому программированию

--- Динамическое программирование/Самостоятельная работа/Декодирование путей/task.py
+++ Динамическое программирование/Самостоятельная работа/Декодирование путей/task.py
@@ -1,20 +1,44 @@
 def num_decodings(s: str) -> int:
     ...
-    d={str(i) for i in range(1,27)}
-    def req(s):
-        i=1
-        while i<len(s):
-            if f'{s[i-1]}{s[i]}' not in d:
-                return req(s[:i])*req(s[i:])
-            i+=1
-        if len(s)==0:
-            return 1
-        if len(1)==1:
-            if s[0] in d:
-                return 1
-            return 0
+    d = {str(i) for i in range(1, 27)}
+    if s[0] == '0' or len(s) == 0:
+        return 0
+    if len(s) == 1:
+        return 1
+    sum = 1
+    fib1 = 1
+    fib2 = 1
+    i = 1
+    while i < len(s):
+        if s[i - 1] + s[i] not in d:
+            sum *= max((fib1),1)
+            fib1 = 1
+            fib2 = 1
+            i += 1
+        elif s[i] == '0':
+            fib1 = 1
+            fib2 = 1
+            i += 1
+        else:
+            fib2, fib1 = fib2 + fib1, fib2
+        i += 1
+    return sum * fib2
 
 
-    return req(s)
+
+print(num_decodings("1201234"))
+print(num_decodings("226"))
+
+print(num_decodings("101"))
 
 print(num_decodings("11106"))
+print(num_decodings("12"))
+print(num_decodings("0"))
+
+
+
+# self.assertEqual(num_decodings("12"), 2)
+# self.assertEqual(num_decodings("226"), 3)
+# self.assertEqual(num_decodings("0"), 0)
+# self.assertEqual(num_decodings("10"), 1)
+# self.assertEqual(num_decodings("101"), 1)
